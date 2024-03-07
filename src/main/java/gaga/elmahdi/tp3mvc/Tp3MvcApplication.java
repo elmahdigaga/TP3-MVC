cc71c437ada3cf04492f4bcb8618fe6f06b14550
elmahdigaga <gaga.elmahdi@gmail.com>
2024-03-07 14:35:40 +0100

    Innitial commit

---
+++ src/main/java/gaga/elmahdi/tp3mvc/Tp3MvcApplication.java
@@ -0,0 +1,13 @@
+package gaga.elmahdi.tp3mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3MvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gaga/elmahdi/tp3mvc/Tp3MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package gaga.elmahdi.tp3mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
