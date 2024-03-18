86d9714c6b3c26b12336a8b336a6f9ebe37d7c16
elmahdigaga <gaga.elmahdi@gmail.com>
2024-03-18 11:44:35 +0000

    feat(controller): Add form related routes

--- src/main/java/gaga/elmahdi/tp3mvc/web/PatientController.java
+++ src/main/java/gaga/elmahdi/tp3mvc/web/PatientController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -39,5 +42,21 @@ public class PatientController {
     @GetMapping("/")
     public String home() {
         return "redirect:/index";
+    }
+
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> listPatients() { return patientRepository.findAll(); }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
+
+    @GetMapping("/save")
+    public String save(Model model, Patient patient) {
+        patientRepository.save(patient);
+        return "formPatients";
     }
 }
